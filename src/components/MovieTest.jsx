aea602f8fb24093372a4bc3f80454b21467f41bb
piperiao <andresfeliperianom@gmail.com>
2024-08-04 19:32:06 -0500

    last try of embedded video

--- src/components/MovieTest.jsx
+++ src/components/MovieTest.jsx
@@ -1,7 +1,7 @@
 export default function MovieTest() {
     return(
         <>
-            <iframe width="600" height="480" src="https://dood.li/e/j45ndptuo49n" scrolling="no" frameborder="0" allowfullscreen="true"></iframe>
+            <iframe width="600" height="480" src="https://drive.google.com/file/d/1-dhiNn8TMvmStEaTnaPLnLhiCj6F2S_e/preview" scrolling="no" frameborder="0" allowfullscreen="true"></iframe>
         </>
     )
 }
